69b866007b141dae7379d696cc1e2e0ab1f41917
Kristin <kristindyulgeryan@abv.bg>
2026-01-21 17:14:31 +0200

    Add Orders page and styles

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage.jsx";
 import CheckoutPage from "./pages/CheckoutPage.jsx";
 import "./App.css";
+import OrderPage from "./pages/OrderPage.jsx";
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrderPage />} />
     </Routes>
   );
 }
