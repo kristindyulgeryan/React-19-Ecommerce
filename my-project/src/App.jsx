f076623cd1c4ab3aabf3368858775ab6fa6489ad
Kristin <kristindyulgeryan@abv.bg>
2026-01-22 10:49:20 +0200

    Add tracking page and styling

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage.jsx";
 import CheckoutPage from "./pages/CheckoutPage.jsx";
 import "./App.css";
 import OrderPage from "./pages/OrderPage.jsx";
+import TrackingPage from "./pages/TrakingPage.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrderPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>
   );
 }
